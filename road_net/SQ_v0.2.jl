2f6f6b3ea2bfdac754a68ed87c81a50b89757e61
Brett Israelsen <brett.israelsen@gmail.com>
2017-11-28 17:07:54 -0700

    adding logging

--- road_net/SQ_v0.2.jl
+++ road_net/SQ_v0.2.jl
@@ -2,19 +2,20 @@ using MCTS
 using Plots, PlotRecipes
 using ProgressMeter
 using JLD
-using Memento # logging utility
+using MicroLogging
+using Base.Markdown
 include("roadnet_pursuer_generator_MDP.jl")
 include("outcome_assessment.jl")
 include("road_net_visualize.jl")
 include("utilities.jl")
 
-
-function run_experiment(g::MetaGraph, mdp::roadnet_with_pursuer, logger::Logger; max_steps::Int64=25,
+function run_experiment(g::MetaGraph, mdp::roadnet_with_pursuer; max_steps::Int64=25,
                         its_vals::Array{Int64}=[5],d_vals::Array{Int64}=[2],num_repeats::Int64=10)
-    info(logger,"$(now()) -- Starting new experiment")
-    info(logger,"running with $g, $mdp, max_steps $max_steps, its_vals $its_vals, d_vals $d_vals, and num_repeats $num_repeats")
     rnet = mdp.road_net.gprops[:net_stats]
     net_diam = rnet.diam
+
+    @info md"# Beginning Experiment"
+    @debug "Using:" max_steps its_vals d_vals num_repeats rnet mdp.tprob mdp.discount mdp.reward_vals mdp.reward_states
 
     # TODO: investigate what the exploration constant does, i.e. does it scale with N?
     # This instantiates empty MCTS policies because MCTS runs "online"
@@ -32,6 +33,7 @@ function run_experiment(g::MetaGraph, mdp::roadnet_with_pursuer, logger::Logger;
             push!(hist_tilde,HistoryRecorder(max_steps=max_steps))
         end
     end
+    @debug "Total number of solvers being tested:" length(policy_tilde)
 
     starting_state = roadnet_pursuer_state(1,4)
 
@@ -48,13 +50,17 @@ function run_experiment(g::MetaGraph, mdp::roadnet_with_pursuer, logger::Logger;
     DA = repmat(d_axis,1,num_repeats)
     utilities = Array{Float64}(size(PT))
     idx = 1
-    p_meter = Progress(length(PT[:]),desc="Calculating Utilities:",color=:yellow,barglyphs=BarGlyphs("[=> ]"))
+    #  p_meter = Progress(length(PT[:]),desc="Calculating Utilities:",color=:yellow,barglyphs=BarGlyphs("[=> ]"))
 
+    @info md"# Simulations"
+    tic()
     for (p,h) in zip(PT,HT)
+        @info "Running Simulations" progress=idx/length(PT)
         utilities[idx] = reward_grab(mdp,p,h,starting_state)
-        ProgressMeter.next!(p_meter)
+        #  ProgressMeter.next!(p_meter)
         idx += 1
     end
+    @info "Completed $(length(PT)) Simulations in $(toq()) seconds"
 
     #  U = reshape(utilities,length(policy_tilde),length(hist_tilde));
     #
@@ -72,23 +78,50 @@ function run_experiment(g::MetaGraph, mdp::roadnet_with_pursuer, logger::Logger;
     #  println("its: $(size(its_axis)), d: $(size(d_axis)), u: $(size(utilities))")
     D = [its_axis d_axis mean(utilities,2)]
     D2 = [IA[:] DA[:] U[:]]
-    violin(D2[:,2],D2[:,3])
+    v = violin(D2[:,2],D2[:,3])
+    if current_logger().default_min_level == MicroLogging.Debug
+        @debug "displaying plot(s)"
+        display(v)
+    else
+        @info "saving plot(s) to file"
+        savefig(v,"test.png")
+    end
     #corrplot(D)
     #  corrplot(D2, labels=["its","d","U"])
     #  scatter3d(its_axis,d_axis,mean(utilities,2)[:])
 end
 
-function main()
+function main(;logtofile::Bool=false, logfname::String="logs/$(now()).log",loglvl::Symbol=:debug)
+    #### Logging
+    if logtofile
+        buffer = IOBuffer()
+        logger = SimpleLogger(buffer)
+        configure_logging(min_level=loglvl)
+    else
+        logger = global_logger()
+        configure_logging(min_level=loglvl)
+    end
+
     g = original_roadnet(exit_rwd=1000.,sensor_rwd=-50.)
-    mdp = roadnet_with_pursuer(g,tp=0.5)
+    mdp = roadnet_with_pursuer(g,tp=0.3)
 
-    logger = Memento.config("info";fmt="[{level} | {name}]: {msg}")
-    add_handler(logger,DefaultHandler("logs/$(now()).log"),"file-logging")
-
-    its_rng = (1., 10000.)
-    d_rng = (1, 2*mdp.road_net.gprops[:net_stats].diam)
+    #  its_rng = (1., 10000.)
+    its_rng = [10]
+    #  d_rng = (1, 2*mdp.road_net.gprops[:net_stats].diam)
+    d_rng = collect(1:10)
     #  its_vals = Int.(round.(latin_hypercube_sampling([its_rng[1]],[its_rng[2]],25)))
     #  d_vals = Int.(round.(latin_hypercube_sampling([d_rng[1]],[d_rng[2]],10)))
 
-    run_experiment(g,mdp,logger)
+    with_logger(logger) do
+        run_experiment(g,mdp,its_vals=its_rng,d_vals=d_rng)
+    end
+
+    if logtofile
+        # using the "open(...) do f ... syntax printed the length of the file at the end, this ways doesn't do that
+        f = open(logfname,"w")
+        logtxt = strip(String(take!(buffer)));
+        logtxt = replace(logtxt, r"^\e\[1m\e\[..m(.- )\e\[39m\e\[22m", s"\1")
+        write(f,logtxt)
+        close(f)
+    end
 end
