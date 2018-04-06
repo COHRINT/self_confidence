9a63564846998f12dce5adae065b0c7e58879c94
Brett Israelsen <brett.israelsen@gmail.com>
2018-04-06 16:55:06 -0600

    added variable number of nodes

--- road_net/make_nets_2vars.jl
+++ road_net/make_nets_2vars.jl
@@ -16,6 +16,7 @@ if fname == "transition_vary"
     train_depth = [8]
     train_deg_bnds = [4.]
     train_net_type = :original
+    train_n_bnds = [13]
 
     num_ok_nets = 250
     ok_t_bnds = [0.0,1.0]
@@ -23,6 +24,7 @@ if fname == "transition_vary"
     ok_depth = [3]
     ok_deg_bnds = [4.]
     ok_net_type = :original
+    ok_n_bnds = [13]
 
     num_bad_nets = 250
     bad_t_bnds = [0.0,1.0]
@@ -30,6 +32,7 @@ if fname == "transition_vary"
     bad_depth = [1]
     bad_deg_bnds = [4.]
     bad_net_type = :original
+    bad_n_bnds = [13]
 elseif fname == "transition_e_vary"
     num_train_nets = 750
     train_t_bnds = [0.0,1.0]
@@ -37,6 +40,8 @@ elseif fname == "transition_e_vary"
     train_depth = [8]
     train_deg_bnds = [4.]
     train_net_type = :original
+    train_n_bnds = [13,45]
+    train_n_bnds = [13]
 
     num_ok_nets = 250
     ok_t_bnds = [0.0,1.0]
@@ -44,6 +49,7 @@ elseif fname == "transition_e_vary"
     ok_depth = [3]
     ok_deg_bnds = [4.]
     ok_net_type = :original
+    ok_n_bnds = [13]
 
     num_bad_nets = 250
     bad_t_bnds = [0.0,1.0]
@@ -51,6 +57,7 @@ elseif fname == "transition_e_vary"
     bad_depth = [1]
     bad_deg_bnds = [4.]
     bad_net_type = :original
+    bad_n_bnds = [13]
 elseif fname == "net_transition_vary"
     num_train_nets = 7
     train_t_bnds = [0.0,1.0]
@@ -58,6 +65,7 @@ elseif fname == "net_transition_vary"
     train_depth = [8]
     train_deg_bnds = [3.,8.]
     train_net_type = :random
+    train_n_bnds = [13,45]
 
     num_ok_nets = 2
     ok_t_bnds = [0.0,1.0]
@@ -65,6 +73,7 @@ elseif fname == "net_transition_vary"
     ok_depth = [3]
     ok_deg_bnds = [3.,8.]
     ok_net_type = :random
+    ok_n_bnds = [13,45]
 
     num_bad_nets = 2
     bad_t_bnds = [0.0,1.0]
@@ -72,24 +81,49 @@ elseif fname == "net_transition_vary"
     bad_depth = [1]
     bad_deg_bnds = [3.,8.]
     bad_net_type = :random
+    bad_n_bnds = [13,45]
+
+elseif fname == "something else"
+    num_train_nets = 7
+    train_t_bnds = [0.0,1.0]
+    train_e_bnds = [1000.]
+    train_depth = [8]
+    train_deg_bnds = [3.,8.]
+    train_net_type = :random
+    train_n_bnds = [13,45]
+
+    num_ok_nets = 2
+    ok_t_bnds = [0.0,1.0]
+    ok_e_bnds = [1000.]
+    ok_depth = [3]
+    ok_deg_bnds = [3.,8.]
+    ok_net_type = :random
+    ok_n_bnds = [13,45]
+
+    num_bad_nets = 2
+    bad_t_bnds = [0.0,1.0]
+    bad_e_bnds = [1000.]
+    bad_depth = [1]
+    bad_deg_bnds = [3.,8.]
+    bad_net_type = :random
+    bad_n_bnds = [13,45]
 end
 
 create_nets= true
 if create_nets
     println("making training solver networks")
-    make_nets(num_train_nets,fname="logs/$(fname)$(train_txt).jld",exit_rwd_bounds=[2000.],sensor_rwd_bounds=[-100.],caught_rwd_bounds=[-2000.],degree_bounds=train_deg_bnds,n_bounds=[13],mcts_its_bounds=[500],mcts_depth_bounds=train_depth,mcts_e_bounds=train_e_bnds,trans_prob_bounds=train_t_bnds,discount_fact_bounds=[0.95],net_type=train_net_type,random_seed=12345)
+    make_nets(num_train_nets,fname="logs/$(fname)$(train_txt).jld",exit_rwd_bounds=[2000.],sensor_rwd_bounds=[-100.],caught_rwd_bounds=[-2000.],degree_bounds=train_deg_bnds,n_bounds=train_n_bnds,mcts_its_bounds=[500],mcts_depth_bounds=train_depth,mcts_e_bounds=train_e_bnds,trans_prob_bounds=train_t_bnds,discount_fact_bounds=[0.95],net_type=train_net_type,random_seed=12345)
 
     println("making test bad solver networks")
-    make_nets(num_bad_nets,fname="logs/$(fname)$(bad_txt).jld",exit_rwd_bounds=[2000.],sensor_rwd_bounds=[-100.],caught_rwd_bounds=[-2000.],degree_bounds=bad_deg_bnds,n_bounds=[13],mcts_its_bounds=[500],mcts_depth_bounds=bad_depth,mcts_e_bounds=bad_e_bnds,trans_prob_bounds=bad_t_bnds,discount_fact_bounds=[0.95],net_type=bad_net_type,random_seed=345)
+    make_nets(num_bad_nets,fname="logs/$(fname)$(bad_txt).jld",exit_rwd_bounds=[2000.],sensor_rwd_bounds=[-100.],caught_rwd_bounds=[-2000.],degree_bounds=bad_deg_bnds,n_bounds=bad_n_bnds,mcts_its_bounds=[500],mcts_depth_bounds=bad_depth,mcts_e_bounds=bad_e_bnds,trans_prob_bounds=bad_t_bnds,discount_fact_bounds=[0.95],net_type=bad_net_type,random_seed=345)
 #
     println("making test ok solver networks")
-    make_nets(num_ok_nets,fname="logs/$(fname)$(ok_txt).jld",exit_rwd_bounds=[2000.],sensor_rwd_bounds=[-100.],caught_rwd_bounds=[-2000.],degree_bounds=ok_deg_bnds,n_bounds=[13],mcts_its_bounds=[500],mcts_depth_bounds=ok_depth,mcts_e_bounds=ok_e_bnds,trans_prob_bounds=ok_t_bnds,discount_fact_bounds=[0.95],net_type=ok_net_type,random_seed=45)
+    make_nets(num_ok_nets,fname="logs/$(fname)$(ok_txt).jld",exit_rwd_bounds=[2000.],sensor_rwd_bounds=[-100.],caught_rwd_bounds=[-2000.],degree_bounds=ok_deg_bnds,n_bounds=ok_n_bnds,mcts_its_bounds=[500],mcts_depth_bounds=ok_depth,mcts_e_bounds=ok_e_bnds,trans_prob_bounds=ok_t_bnds,discount_fact_bounds=[0.95],net_type=ok_net_type,random_seed=45)
 end
 
 println("creating training data")
 make_simulations = true
 if make_simulations
-
     make_training_data(data_fname="logs/$(fname)$(train_txt)",repeats=250,sim_steps=-1,dis_rwd=false)
     make_training_data(data_fname="logs/$(fname)$(bad_txt)",repeats=250,sim_steps=-1,dis_rwd=false)
     make_training_data(data_fname="logs/$(fname)$(ok_txt)",repeats=250,sim_steps=-1,dis_rwd=false)
