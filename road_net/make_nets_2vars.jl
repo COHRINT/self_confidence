bd2895e94ce67e3dfaf68f6ac883c8aafb2d626f
Brett Israelsen <brett.israelsen@gmail.com>
2018-04-12 19:58:23 +0000

    update generated networks

--- road_net/make_nets_2vars.jl
+++ road_net/make_nets_2vars.jl
@@ -2,8 +2,9 @@
 @everywhere importall POMDPs, POMDPToolbox
 @everywhere include("make_training_nets.jl")
 @everywhere include("make_training_data_SQ.jl")
+@everywhere include("send_mail.jl")
 
-fname = "net_transition_vary"
+fname = "net_transition_vary2"
 train_txt = "_reference_solver_training"
 test_txt = "_reference_solver_test"
 bad_txt = "_bad_solver"
@@ -83,11 +84,36 @@ elseif fname == "net_transition_vary"
     bad_net_type = :random
     bad_n_bnds = [13,45]
 
+elseif fname == "net_transition_vary2"
+    num_train_nets = 750
+    train_t_bnds = [0.6,1.0]
+    train_e_bnds = [1000.]
+    train_depth = [8]
+    train_deg_bnds = [3.,8.]
+    train_net_type = :random
+    train_n_bnds = [13,45]
+
+    num_ok_nets = 250
+    ok_t_bnds = [0.6,1.0]
+    ok_e_bnds = [1000.]
+    ok_depth = [3]
+    ok_deg_bnds = [3.,8.]
+    ok_net_type = :random
+    ok_n_bnds = [13,45]
+
+    num_bad_nets = 250
+    bad_t_bnds = [0.6,1.0]
+    bad_e_bnds = [1000.]
+    bad_depth = [1]
+    bad_deg_bnds = [3.,8.]
+    bad_net_type = :random
+    bad_n_bnds = [13,45]
+
 elseif fname == "something else"
     #keeping empty for now
 end
 
-create_nets= true
+create_nets= false
 if create_nets
     println("making training solver networks")
     make_nets(num_train_nets,fname="logs/$(fname)$(train_txt).jld",exit_rwd_bounds=[2000.],sensor_rwd_bounds=[-100.],caught_rwd_bounds=[-2000.],degree_bounds=train_deg_bnds,n_bounds=train_n_bnds,mcts_its_bounds=[500],mcts_depth_bounds=train_depth,mcts_e_bounds=train_e_bnds,trans_prob_bounds=train_t_bnds,discount_fact_bounds=[0.95],net_type=train_net_type,random_seed=12345)
@@ -105,4 +131,12 @@ if make_simulations
     make_training_data(data_fname="logs/$(fname)$(train_txt)",repeats=250,sim_steps=-1,dis_rwd=false)
     make_training_data(data_fname="logs/$(fname)$(bad_txt)",repeats=250,sim_steps=-1,dis_rwd=false)
     make_training_data(data_fname="logs/$(fname)$(ok_txt)",repeats=250,sim_steps=-1,dis_rwd=false)
+end
+
+if on_gcloud()
+    #if we're on Google cloud, then send an email when code is done
+    hname = on_gcloud(return_name=true)
+    body = "The code on $hname has finished running"
+    subject = "Done running code on $hname"
+    send_mail(subject,body)
 end
