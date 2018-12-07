3eef11e55c9f1477a09996ef1c336b9985886e06
Brett Israelsen <brett.israelsen@gmail.com>
2018-12-07 16:38:33 -0700

    Fixed error in how success/fail was calculated. set random seed to make results consistent. removed plotting...i'll take care of that somewhere else.

--- road_net/make_experiment_data.jl
+++ road_net/make_experiment_data.jl
@@ -28,6 +28,11 @@ end
 
 if run[:json]
     info("## Making .json file for experiment data")
+    srand(22222) # for consistent results
+    # 222 has 21s, 24f
+    # 2 has 24 successes and 21 failures, distribution looks OK too
+    # 999990 has 21 success and 24 failures, but seems to be lucky when xP is low and xQ is high
+    # 999 has 18 successes
     for expr in keys(exp_dict[:conditions])
         net_type = exp_dict[:name]
         inpts = exp_dict[:xQ][:inpts]
@@ -40,15 +45,6 @@ if run[:json]
 
         exp_json = Dict()
 
-        # variables for making diagnostic plots
-        # task set is the set that will be used in the experiment, it is based off of
-        # looking at each of the graphs "by hand", and choosing ones that "look good"
-        # (i.e. aren't too complicated, truck/motorcycle don't start too close)
-        task_set = [100,10,13,16,18,22,23,25,29,30,32,39,41,43,44,48,49,50,54,56,57,58,59,60,62,66,6,74,75,76,77,79,7,80,82,84,87,89,8,90,91,93,94,97,98]
-        q = []
-        p = []
-        oc = []
-
         for net in d
             n = exp_json[net[1]] = Dict()
             net_vals = net[2]
@@ -60,44 +56,36 @@ if run[:json]
             # this will have a higher probability of drawing most repeated numbers, so it is like sampling
             # a parameterized distribution, with the limitaion that it will only draw an outcome that has
             # already been seen
-            # If that value is greater than the mean expected reward (:X3_1) then the experiment is a success
+            # If that value is greater than the mean expected reward of the trusted distribution (:X3_1) then the experiment is a success
             # otherwise it is a failure
             r_dist = net_vals[:training_data][:r_dist]
             myval = r_dist
             o = rand(r_dist)
-            train_mean = net_vals[:training_data][:X3_1]
-            train_mean > o ? (outcome = "fail") : (outcome = "success")
-            #  info("outcome: $o")
-            #  info("train_mean: $train_mean")
-            #  info("outcome assessment: $(n[:xP])")
-            n[:outcome] = outcome
-            outcome == "success" ? oc_bool = 1 : oc_bool = 0
-            #  info(n[:outcome])
 
-            if net[1] ∈ task_set
-                push!(q,n[:xQ])
-                push!(p,n[:xP])
-                push!(oc,oc_bool)
+            #  r_star_mean = net_vals[:training_data][:X3_1] # Old, WRONG way, leave hear for illustrative purposes
+            r_star_mean = net_vals[:solver_quality][:R_star][:X3_1]
+            if all(r_star_mean .> o) && all(o .< 0.0)
+                outcome = "fail"
+            else
+                outcome = "success"
             end
+            n[:outcome] = outcome
+            n[:num_nodes] = net_vals[:num_nodes]
+            n[:trans_prob] = net_vals[:trans_prob]
+            n[:rwd_draw] = o
+            n[:trusted_mean] = r_star_mean
+            outcome == "success" ? oc_bool = 1 : oc_bool = 0
+            #  println("rand draw: $o")
+            #  println("trusted mean: $r_star_mean")
+            #  println("oucome: $outcome")
+            #  readline()
+            #  info(n[:outcome])
 
             n[:image_file] = joinpath(net_vals[:img_name])
             n[:mcts_depth] = net_vals[:mcts_depth]
         end
-        if false
-            # make some diagnostic plots to check out the results
-            fig,ax = PyPlot.subplots(3,1)
-            ax[1][:scatter](q[oc.==1],p[oc.==1],label="success $(length(oc[oc.==1]))")
-            ax[1][:scatter](q[oc.==0],p[oc.==0],label="fail $(length(oc[oc.==0]))")
-            ax[1][:legend]()
-            ax[2][:hist](q[oc.==1])
-            ax[2][:hist](q[oc.==0])
-            ax[3][:hist](p[oc.==1])
-            ax[3][:hist](p[oc.==0],label="p<0 $(length(p[p.<0.]))")
-            ax[3][:legend]()
-            readline()
-        end
 
-        json_fname = "logs/experiment_data_$fname.json"
+        json_fname = "logs/experiment_data_$(fname).json"
         info("Writing to $json_fname")
         open(json_fname,"w") do f
             dat = JSON.json(exp_json,4)
