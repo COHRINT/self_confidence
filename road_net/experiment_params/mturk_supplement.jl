2f4250946f1861b767fd0f1f50c59686a6153922
Brett Israelsen <brett.israelsen@gmail.com>
2018-12-13 12:40:12 -0700

    need to make a supplemental data set for more mturk data

---
+++ road_net/experiment_params/mturk_supplement.jl
@@ -0,0 +1,13 @@
+# vary transition prob, mcts_depth, and number of nodes in network
+cond1 = make_condition_dict(num_nets=500,mcts_d=[10],n_bnds=[8,35],seed=5,role=:trusted)
+cond4 = make_condition_dict(num_nets=100,mcts_d=[3],n_bnds=[8,35],seed=2)
+
+exp_dict = Dict(:name=>"mturk_supplement", :conditions=>Dict(1=>cond1, 4=> cond4),
+                :xQ=>Dict(:inpts=>[:tprob,:N],:epocs=>1000,:cmp=>[4],:nn_loc=>"nn_logs"))
+
+fldr = "logs"
+for x in keys(exp_dict[:conditions])
+    fname = make_useful_fname(exp_dict,x)
+    exp_dict[:conditions][x][:fname] = fname
+    exp_dict[:conditions][x][:fldr] = fldr
+end
