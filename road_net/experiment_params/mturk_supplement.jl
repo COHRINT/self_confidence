95dd70a9dae6386439b4b6df12ee11c0d170971d
Brett Israelsen <brett.israelsen@gmail.com>
2018-12-13 13:13:34 -0700

    need 4th condition to have 500 networks

--- road_net/experiment_params/mturk_supplement.jl
+++ road_net/experiment_params/mturk_supplement.jl
@@ -1,6 +1,6 @@
 # vary transition prob, mcts_depth, and number of nodes in network
 cond1 = make_condition_dict(num_nets=500,mcts_d=[10],n_bnds=[8,35],seed=5,role=:trusted)
-cond4 = make_condition_dict(num_nets=100,mcts_d=[3],n_bnds=[8,35],seed=2)
+cond4 = make_condition_dict(num_nets=500,mcts_d=[3],n_bnds=[8,35],seed=2)
 
 exp_dict = Dict(:name=>"mturk_supplement", :conditions=>Dict(1=>cond1, 4=> cond4),
                 :xQ=>Dict(:inpts=>[:tprob,:N],:epocs=>1000,:cmp=>[4],:nn_loc=>"nn_logs"))
